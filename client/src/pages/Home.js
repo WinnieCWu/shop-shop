0e4527aead9c5eafac8f63d4dc0b24738385758a
WinnieCWu <winniechn7@gmail.com>
2022-06-12 17:44:57 -0700

    refactored detail and home pages, along with product list, so it uses global state

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -3,13 +3,14 @@ import ProductList from "../components/ProductList";
 import CategoryMenu from "../components/CategoryMenu";
 
 const Home = () => {
-  const [currentCategory, setCategory] = useState("");
+  // const [currentCategory, setCategory] = useState("");
+    //above code unneeded since state is globally managed now
   //Home pg manages state current and setCategory, which is passed to ProductList comp as a prop
   //products from selected category in ProductList is retrieved using Apollo, then passed as new via setCategory
   return (
     <div className="container">
-      <CategoryMenu setCategory={setCategory} />
-      <ProductList currentCategory={currentCategory} />
+      <CategoryMenu />
+      <ProductList />
     </div>
   );
 };
