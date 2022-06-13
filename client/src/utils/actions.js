06542a2fc7ce3a12b36c07d67686fcb8b6161a3f
WinnieCWu <winniechn7@gmail.com>
2022-06-13 12:30:27 -0700

    wrote and ran tests for shopping cart reducers for related actions

--- client/src/utils/actions.js
+++ client/src/utils/actions.js
@@ -7,3 +7,10 @@ export const UPDATE_CATEGORIES = "UPDATE_CATEGORIES";
 export const UPDATE_CURRENT_CATEGORY = "UPDATE_CURRENT_CATEGORY";
 //connecting piece. we can select category from state and display it in the products
 
+//add actions related to shopping cart
+export const ADD_TO_CART = 'ADD_TO_CART';
+export const ADD_MULTIPLE_TO_CART = 'ADD_MULTIPLE_TO_CART';
+export const REMOVE_FROM_CART = 'REMOVE_FROM_CART';
+export const UPDATE_CART_QUANTITY = 'UPDATE_CART_QUANTITY';
+export const CLEAR_CART = 'CLEAR_CART';
+export const TOGGLE_CART = 'TOGGLE_CART';
--- client/src/utils/reducers.js
+++ client/src/utils/reducers.js
@@ -1,45 +1,102 @@
-import { useReducer } from 'react';
+import { useReducer } from "react";
 
 import {
-    UPDATE_PRODUCTS,
-    UPDATE_CATEGORIES,
-    UPDATE_CURRENT_CATEGORY
-  } from "./actions";
+  UPDATE_PRODUCTS,
+  UPDATE_CATEGORIES,
+  UPDATE_CURRENT_CATEGORY,
+  ADD_TO_CART,
+  ADD_MULTIPLE_TO_CART,
+  REMOVE_FROM_CART,
+  UPDATE_CART_QUANTITY,
+  CLEAR_CART,
+  TOGGLE_CART,
+} from "./actions";
 
-  //creates a fxn called reducer. when executed, it'll pass value of action.type argument into switch statement
-  // and compare with possible actions
+//creates a fxn called reducer. when executed, it'll pass value of action.type argument into switch statement
+// and compare with possible actions
 export const reducer = (state, action) => {
-    switch (action.type) {
-      // if action type value is the value of `UPDATE_PRODUCTS`, return a new state object with an updated products array
-      case UPDATE_PRODUCTS:
-          //return new obj with copy of state argument using spread operator
-        return {
-          ...state,
-          products: [...action.products],
-        };
+  switch (action.type) {
+    // if action type value is the value of `UPDATE_PRODUCTS`, return a new state object with an updated products array
+    case UPDATE_PRODUCTS:
+      //return new obj with copy of state argument using spread operator
+      return {
+        ...state,
+        products: [...action.products],
+      };
 
-      // if action type value is the value of `UPDATE_CATEGORIES`, return a new state object with an updated categories array
-      case UPDATE_CATEGORIES:
-        return {
-          ...state,
-          categories: [...action.categories]
-        };
+    // if action type value is the value of `UPDATE_CATEGORIES`, return a new state object with an updated categories array
+    case UPDATE_CATEGORIES:
+      return {
+        ...state,
+        categories: [...action.categories],
+      };
+
+    case UPDATE_CURRENT_CATEGORY:
+      return {
+        ...state,
+        currentCategory: action.currentCategory,
+      };
+
+    case ADD_TO_CART:
+      return {
+        ...state,
+        cartOpen: true,
+        cart: [...state.cart, action.product],
+      };
+
+    case ADD_MULTIPLE_TO_CART:
+      return {
+        ...state,
+        cart: [...state.cart, ...action.products],
+      };
+
+    //filter only keeps items not matching the provided _id property
+    case REMOVE_FROM_CART:
+      let newState = state.cart.filter((product) => {
+        return product._id !== action._id;
+      });
+
+      return {
+        ...state,
+        cartOpen: newState.length > 0,
+        cart: newState,
+      };
+
+    case UPDATE_CART_QUANTITY:
+      return {
+        ...state,
+        cartOpen: true,
+        //initial state is immutable, so don't update state.cart
+        cart: state.cart.map((product) => {
+          if (action._id === product._id) {
+            product.purchaseQuantity = action.purchaseQuantity;
+          }
+          return product;
+        }),
+      };
+
+    case CLEAR_CART:
+      return {
+        ...state,
+        cartOpen: false,
+        cart: [],
+      };
+
+    case TOGGLE_CART:
+      return {
+        ...state,
+        cartOpen: !state.cartOpen,
+      };
       
-      case UPDATE_CURRENT_CATEGORY:
-        return {
-          ...state,
-          currentCategory: action.currentCategory
-        };
-
-      // if it's none of these actions, do not update state at all and keep things the same!
-      default:
-        return state;
-    }
-  };
-
-  //this fxn will be used to help initalize our global state obj and 
-  //then provide us with fxnality for updating that state by auto running it thru reducer() fxnality
-  //its a more in-depth way of using the useState() Hook
-  export function useProductReducer(initialState) {
-    return useReducer(reducer, initialState);
+    // if it's none of these actions, do not update state at all and keep things the same!
+    default:
+      return state;
   }
+};
+
+//this fxn will be used to help initalize our global state obj and
+//then provide us with fxnality for updating that state by auto running it thru reducer() fxnality
+//its a more in-depth way of using the useState() Hook
+export function useProductReducer(initialState) {
+  return useReducer(reducer, initialState);
+}
